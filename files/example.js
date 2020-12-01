af22fd2058a4ab0c741a407754e225a5fb228ca2
g33na <73322304+g33na@users.noreply.github.com>
2020-12-01 00:17:39 -1000

    creation of hide and show buttons to demo js functions

--- files/example.js
+++ files/example.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
-    console.log("ready!");
+$( document ).ready(function () {
+    console.log( "ready!" );
+/*     $("button").on("click", function () {
+        console.log("click working");
+        $('a').hide();
+    });  */
+    $( "[function=hide]" ).on( "click", function () {
+        console.log("click to hide");
+        $('a').fadeOut();
+    }); 
+    $( "[function=show]" ).on( "click", function () {
+        console.log("click to show");
+        $('a').fadeIn();
+    });  
 });
